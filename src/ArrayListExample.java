79a699e7c01c174f75f8331b8f82f5624f560c14
gguilhermelopes <guilhermelopes275@gmail.com>
2023-06-16 11:57:05 -0300

    arraylist

---
+++ src/ArrayListExample.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+    public void exemploListaSimples(){
+        List<String> list = new ArrayList<>();
+        list.add("Guilherme");
+        list.add("Buken");
+        list.add("Fubá");
+        System.out.println(list);
+    }
+}
