844f6efad194e9ffe8ec853cad1549de15c8a101
gguilhermelopes <guilhermelopes275@gmail.com>
2023-06-19 14:50:28 -0300

    exemplos arrayList

---
+++ src/Aluno.java
@@ -0,0 +1,48 @@
+public class Aluno {
+    private String nome;
+    private String curso;
+    private double nota;
+    private boolean aprovado;
+
+    public Aluno(String nome, String curso, double nota) {
+        this.nome = nome;
+        this.curso = curso;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public boolean isAprovado() {
+        return aprovado;
+    }
+
+    public void setAprovado(boolean aprovado) {
+        this.aprovado = aprovado;
+    }
+
+    public String toString(){
+        return this.nome;
+    }
+}
--- src/ArrayListExample.java
+++ src/ArrayListExample.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ArrayListExample {
@@ -7,6 +8,28 @@ public class ArrayListExample {
         list.add("Guilherme");
         list.add("Buken");
         list.add("Fubá");
+        Collections.sort(list);
         System.out.println(list);
+
+        List<Integer> listInt = new ArrayList<Integer>();
+        listInt.add(1);
+        listInt.add(2);
+        listInt.add(3);
+        listInt.add(4);
+        Collections.sort(listInt);
+        Collections.reverse(listInt);
+        System.out.println(listInt);
+
+        List<Aluno> alunos = new ArrayList<Aluno>();
+
+        Aluno a = new Aluno("Guilherme", "Java", 8);
+        Aluno b = new Aluno("Buken", "JavaScript", 10);
+
+        alunos.add(a);
+        alunos.add(b);
+
+        System.out.println(alunos);
+
+
     }
 }
